bebbca9b3ab080ea19bcda1bb0a418608e50572c
zhangjiaw <1617386850@qq.com>
2021-11-13 23:36:36 +0800

    init

---
+++ upgrade/src/main/java/com/upgrade/UpgradeMain.java
@@ -0,0 +1,15 @@
+package com.upgrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author zhangjiaw
+ * @Date 2021/11/13 23:12
+ */
+@SpringBootApplication
+public class UpgradeMain {
+    public static void main(String[] args) {
+        SpringApplication.run(UpgradeMain.class);
+    }
+}
