2a9c41fd3e3570ec830786bc416d2e76c903123a
zhangjiaw <1617386850@qq.com>
2022-12-03 14:35:33 +0800

    Revert "redis限流"
    This reverts commit 557585fe

--- upgrade/target/classes/lua/limit.lua
+++
@@ -1,7 +0,0 @@
-local key = KEYS[1]
-local period = tonumber(ARGV[1])
-local current = redis.call('incr', key)
-if tonumber(current) == 1 then
-    redis.call('expire', key, period)
-end
-return tonumber(current)
